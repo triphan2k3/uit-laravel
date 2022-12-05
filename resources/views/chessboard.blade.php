3d830e86ab92b721ae42999c4fdb9fe4ea580173
nttlan <emeraldlan1105@gmail.com>
2022-12-05 11:44:54 +0700

    fix feature

--- resources/views/chessboard.blade.php
+++ resources/views/chessboard.blade.php
@@ -10,63 +10,53 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200" id="div1">
-                    <div id="board" style="width: 400px"></div>
-                    <button id="startBtn" style='color:red'>Start Position</button>
-                    <button id="clearBtn" style='color:green' >Clear Board</button>
-                    <button id="showPositionInConsoleBtn" style='color:aqua'>Show position in console</button>
-                    <button id="showPositionBtn" style='color:aqua'>Show position</button>
-                    <div id="positionStr" style="word-wrap: break-word"></div>
+                    <div style="display: flex; flex-direction: row; align-items: center; justify-content: space-around;">
+                        <div id="board" style="width: 400px"></div>
+                        <ul id="moveList"></ul>
+                    </div>
+                    <button class="btn btn-success mt-2" id="startBtn">Start Position</button>
+                    <button class="btn btn-danger mt-2" id="clearBtn">Clear Board</button>
+                    <button class="btn btn-primary mt-2" id="showPositionBtn">Show position in console</button>
                     <script>
                         // Init a chessboard
-                        let board = Chessboard('board', {
+                        var config = {
                             draggable: true,
                             dropOffBoard: 'snapback',
-                            position: 'start'
-                        })
-
-                        let previousPosition = board.position()
-                        function displayPlayerMove() {
-                            let newPosition = board.position()
-                            let currentPiece;
-
-                            //find piece that moved
-                            for (let i = 0; i < Object.keys(previousPosition).length; i++) {
-                                const pos = Object.keys(previousPosition)[i]
-                                if (newPosition[pos] == previousPosition[pos])
-                                    continue
-                                else if (newPosition[pos] == undefined && previousPosition[pos] != undefined) {
-                                    currentPiece = previousPosition[pos]
-                                    // console.log(currentPiece)
-                                    break
-                                }
-                            }
-
-                            //find position
-                            for (let j = 0; j < Object.keys(newPosition).length; j++) {
-                                const pos1 = (Object.keys(newPosition))[j]
-                                if (newPosition[pos1] == currentPiece && previousPosition[pos1] == undefined) {
-                                    console.log(currentPiece + "/" + pos1)
-                                    break
-                                }
-                            }
-
-                            previousPosition = newPosition
+                            position: 'start',
+                            onDrop: onDrop,
                         }
 
+                        var board = Chessboard('board', config)
 
                         // Button event
                         $('#startBtn').on('click', board.start)
                         $('#clearBtn').on('click', board.clear)
-                        $('#showPositionInConsoleBtn').on('click', displayPlayerMoveInConsole)
-                        $('#showPositionBtn').on('click', displayPlayerMove)
-                        // $('body > img').on('drag', displayPlayerMove)
+                        $('#showPositionBtn').on('click', clickShowPositionBtn)
 
-
-                        function displayPlayerMoveInConsole() {
+                        function clickShowPositionBtn () {
+                            console.log('Current position as an Object:')
                             console.log(board.position())
+
                             console.log('Current position as a FEN string:')
                             console.log(board.fen())
-                            console.log(Object.keys(board.position()))
+                        }
+
+                        function onDrop (source, target, piece, newPos, oldPos, orientation) {
+                            console.log('Source: ' + source)
+                            console.log('Target: ' + target)
+                            console.log('Piece: ' + piece)
+                            console.log('Orientation: ' + orientation)
+                            let data = '♟'+ " " + target
+                            addListItem(data)
+                        }
+
+                        function addListItem(data) {
+                            let ul = document.getElementById('moveList')
+                            let li = document.createElement('li')
+                            ul.appendChild(li)
+                        
+                            let list = document.querySelector('li')
+                            list.innerHTML = data
                         }
                         
                     </script>
